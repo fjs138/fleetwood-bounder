f2e7f107791488efd985f921c9e3b2677ad88fd4
Frank Santaguida <fjs138@gmail.com>
2020-09-22 21:33:41 -0400

    cosmetic changes

---
+++ src/components/ui/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from '../../img/logo.png'
+//import logo2 from '../../img/logo2.png'
+
+function Footer(){
+  return (
+      <header className={'center'}>
+        <siteDescription>Fleetwood Bounder is a searchable gallery of the casts of AMC's
+          award winning shows Breaking Bad and Better Call Saul</siteDescription>
+      </header>
+  )
+}
+
+export default Footer;
